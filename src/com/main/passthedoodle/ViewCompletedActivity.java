9bfaf2c46f1f13b7ebed68fa1098158060dd7893
dknguyen <dknguyen88@gmail.com>
2013-12-04 15:50:34 -0800

    semi view completed activity from db
    -submitting what i have now (not working of course)

--- src/com/main/passthedoodle/ViewCompletedActivity.java
+++ src/com/main/passthedoodle/ViewCompletedActivity.java
@@ -2,13 +2,25 @@ package com.main.passthedoodle;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -24,6 +36,23 @@ public class ViewCompletedActivity extends FragmentActivity {
 	ViewCompletedPagerAdapter mViewCompletedPagerAdapter;
 	ImageLoader imageLoader;
 	DisplayImageOptions options;
+	
+	// Progress Dialog
+    private ProgressDialog pDialog;
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+    //ArrayList<HashMap<String, String>> gamesList;
+    // url to get all games list
+    private static String url_list_games = "http://passthedoodle.com/test/get_comp_game.php";
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_GAME = "game";
+    //private static final String TAG_USERNAME = "username";
+    //private static final String TAG_SEQ = "sequence";
+    private static final String TAG_DESC = "description";
+    private static final String TAG_FILENAME = "filename";
+    // games JSONArray
+    JSONArray game = null;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +92,26 @@ public class ViewCompletedActivity extends FragmentActivity {
             // - implement it in mViewPager
             
             // game_id is set to 100 for now from MainActivity
-            String gameID = getIntent().getStringExtra("game_id");
+            String game_id = getIntent().getStringExtra("game_id");
+            new LoadCompGame().execute(game_id);
+            // looping through All Games
+            int gameLength = game.length();
+            for (int i = 0; i < gameLength-1; i+=2) {
+                String url;
+                String prompt;
+                String desc;
+                try {
+                    url = "http://passthedoodle.com/i/" + game.getJSONObject(i+1).getString(TAG_DESC);
+                    prompt = game.getJSONObject(i).getString(TAG_FILENAME);
+                    desc = game.getJSONObject(i+2).getString(TAG_DESC);
+                    if (i <= gameLength-1)
+                        stringsList.add(new RoundInfo(url, prompt, desc));
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                
+            }
             /* Implement method to retrieve from db
              * for every drawing of gameID
                     RoundInfo.imageUrl <- image urls
@@ -135,6 +183,103 @@ public class ViewCompletedActivity extends FragmentActivity {
             return (i*2 + 1) + str;
         }
     }
+	
+	/**
+	 * Background Async Task to Load Completed Game description and images by making HTTP Request
+	 * */
+	class LoadCompGame extends AsyncTask<String, String, String> {
+
+	    /**
+	     * Before starting background thread Show Progress Dialog
+	     * */
+	    @Override
+	    protected void onPreExecute() {
+	        super.onPreExecute();
+	        ProgressDialog pDialog = new ProgressDialog(getApplicationContext());
+	        pDialog.setMessage("Loading games. Please wait...");
+	        pDialog.setIndeterminate(false);
+	        pDialog.setCancelable(false);
+	        pDialog.show();
+	    }
+
+	    /**
+	     * getting All games from url
+	     * */
+	    protected String doInBackground(String... args) {
+	        // Building Parameters
+	        List<NameValuePair> params = new ArrayList<NameValuePair>();
+	        SharedPreferences pref = getSharedPreferences("ptd", 0);
+	        String session = pref.getString("session", "0");
+	        params.add(new BasicNameValuePair("PHPSESSID", session));
+	        params.add(new BasicNameValuePair("game_id", getIntent().getStringExtra("game_id")));
+	        // getting JSON string from URL
+	        JSONObject json = jParser.makeHttpRequest(url_list_games, "POST", params);
+	        // Check your log cat for JSON response
+	        Log.d("All Games: ", json.toString());
+	        
+	        try {
+	            // Checking for SUCCESS TAG
+	            int success = json.getInt(TAG_SUCCESS);
+
+	            if (success == 1) {
+	                // games found
+	                // Getting Array of Games
+	                game = json.getJSONArray(TAG_GAME);
+	            } else if (success == 0) {
+	                return "0";
+	            } else if (success == 2) {
+	                return "2";
+	            }
+	        } catch (JSONException e) {
+	            e.printStackTrace();
+	            System.out.print("JSON Exception occurred");
+	            Toast.makeText(getApplicationContext(), "JSON Error occurred. Try logging in again.", Toast.LENGTH_LONG).show();
+	        } catch (NullPointerException n) {
+	            n.printStackTrace();
+	            System.out.print("Null Pointer Exception occurred");
+	            Toast.makeText(getApplicationContext(), "Null Error occurred. Try logging in again.", Toast.LENGTH_LONG).show();
+	        } catch (RuntimeException r) {
+	            r.printStackTrace();
+	            System.out.print("Runtime Exception occurred");
+	            Toast.makeText(getApplicationContext(), "Runtime Error occurred. Try logging in again.", Toast.LENGTH_LONG).show();
+	        }
+	        return null;
+	    }
+
+	    /**
+	     * After completing background task Dismiss the progress dialog
+	     * **/
+	    protected void onPostExecute(String file_url) {
+	        // dismiss the dialog after getting all games
+	        pDialog.dismiss();
+	        // updating UI from Background Thread
+	        if (file_url != null) {
+	            if (file_url.equals("0")) {
+	                Toast.makeText(getApplicationContext(), "Game not found.", Toast.LENGTH_LONG).show();
+	            } else {
+	                Toast.makeText(getApplicationContext(), "Log in or register please.", Toast.LENGTH_LONG).show();
+	                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+	                // Closing all previous activities
+	                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	                startActivity(i);
+	            }
+	        }
+	        //((Activity) getApplicationContext()).runOnUiThread(new Runnable() {
+	        //    public void run() {
+	                /**
+	                 * Updating parsed JSON data into ListView
+	                 * */
+/*	                ListAdapter adapter = new SimpleAdapter(
+	                        getApplicationContext(), gamesList,
+	                        R.layout.item_layout, new String[] { TAG_ID, TAG_NAME, TAG_FILENAME, TAG_DESC, TAG_ICON },
+	                        new int[] { R.id.id, R.id.name, R.id.filename, R.id.description, R.id.icon });
+	                // updating listview
+	                setListAdapter(adapter);
+	            }
+	        });*/
+	    }
+	}
+
 }
 
 class DepthPageTransformer implements ViewPager.PageTransformer {
